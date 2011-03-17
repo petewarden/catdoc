37af16c22cfaed7d1fc58a2562ef193313a9ca4e
Pete Warden <pete@petewarden.com>
2011-03-17 08:10:51 -0700

    Initial import

---
+++ src/ppt.h
@@ -0,0 +1,22 @@
+/**
+ * @file   ppt.h
+ * @author Alex Ott <alexott@gmail.com>
+ * @date   23 ‰≈À 2004
+ * Version: $Id: ppt.h,v 1.1.1.1 2006/02/24 17:44:06 vitus Exp $
+ * Copyright: Alex Ott
+ * 
+ * @brief  definitions of .ppt processing functions
+ * 
+ * 
+ */
+
+#ifndef _PPT_H
+#define _PPT_H 1
+
+#include <stdio.h>
+#include <math.h>
+
+void do_ppt(FILE *input,char *filename);
+
+#endif /* _PPT_H */
+
